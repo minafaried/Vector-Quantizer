f9d84e3e3a0623a3ae5737063265fbe68445054a
mina faried <36136028+minafaried@users.noreply.github.com>
2019-12-16 14:36:09 +0200

    Update vector.java

--- vector.java
+++ vector.java
@@ -16,12 +16,14 @@ public class vector {
 
 
 	public void dispaly() {
+		
 		for (int i = 0; i < w; i++) {
 			for (int j = 0; j < h; j++) {
 				System.out.print(this.v[i][j]+" ");
 			}
 			System.out.println();
 		}
+		System.out.println("-------");
 	}
 	
 }
